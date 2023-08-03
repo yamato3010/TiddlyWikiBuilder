020eefb8b5462a34fb2fc82fb40b297a509bcd23
yamato3010 <yama3to10yama3@gmail.com>
2023-08-03 22:05:22 +0900

    動的ページに飛ぶためのリンクを追加

--- insertNotification.py
+++ insertNotification.py
@@ -11,15 +11,19 @@ def add_text_after_body(html_file, text_to_add):
     with open(html_file, 'w', encoding='utf-8') as file:
         file.write(modified_content)
 
-def process_html_files(directory, text_to_add):
+def process_html_files(directory):
     for root, dirs, files in os.walk(directory):
         for file in files:
             if file.endswith('.html'):
                 file_path = os.path.join(root, file)
+                base_name = os.path.basename(file_path)
+                filename, _ = os.path.splitext(base_name)
+                text_to_add = '<p style="margin-left: 10px;">このページは<a href="https://yamato3010.github.io/wiki">大和ノ求聞史Wiki</a>の静的バージョンです。記事の内容が最新でない可能性があります。動的版は<a href="https://yamato3010.github.io/wiki#' + filename + '">こちら</a></p>'
                 add_text_after_body(file_path, text_to_add)
+                
 
 if __name__ == "__main__":
     directory_path = "./myfirstwiki/output/static"  # ディレクトリのパスを指定してください
     text_to_add = '<p style="margin-left: 10px;">このページは<a href="https://yamato3010.github.io/wiki">大和ノ求聞史Wiki</a>の静的バージョンです。記事の内容が最新でない可能性があります。</p>'
 
-    process_html_files(directory_path, text_to_add)
+    process_html_files(directory_path)
