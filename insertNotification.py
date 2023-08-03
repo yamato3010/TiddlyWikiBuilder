c1ee02b06e7a4efc7754142ede6cc6e09843d9a2
yamato3010 <yama3to10yama3@gmail.com>
2023-08-03 21:36:47 +0900

    自動で，静的サイトであることの通知を記述するように

---
+++ insertNotification.py
@@ -0,0 +1,25 @@
+import os
+import re
+
+def add_text_after_body(html_file, text_to_add):
+    with open(html_file, 'r', encoding='utf-8') as file:
+        content = file.read()
+
+    # bodyタグの直後にテキストを追加
+    modified_content = re.sub(r'(<body.*?>)', r'\1\n<p>{}</p>'.format(text_to_add), content)
+
+    with open(html_file, 'w', encoding='utf-8') as file:
+        file.write(modified_content)
+
+def process_html_files(directory, text_to_add):
+    for root, dirs, files in os.walk(directory):
+        for file in files:
+            if file.endswith('.html'):
+                file_path = os.path.join(root, file)
+                add_text_after_body(file_path, text_to_add)
+
+if __name__ == "__main__":
+    directory_path = "./myfirstwiki/output/static"  # ディレクトリのパスを指定してください
+    text_to_add = '<p style="margin-left: 10px;">このページは<a href="https://yamato3010.github.io/wiki">大和ノ求聞史Wiki</a>の静的バージョンです。記事の内容が最新でない可能性があります。</p>'
+
+    process_html_files(directory_path, text_to_add)
