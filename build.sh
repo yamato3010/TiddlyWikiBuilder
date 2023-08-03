acf5a58d035208d30803f08ce4df189096df5592
Yamato <75778273+yamato3010@users.noreply.github.com>
2023-08-03 21:14:05 +0900

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,4 +4,6 @@ cd myfirstwiki&&
 tiddlywiki --rendertiddlers '[!is[system]]' $:/core/templates/static.tiddler.html static text/plain&&
 tiddlywiki --rendertiddler $:/core/templates/static.template.css static/static.css text/plain&&
 tiddlywiki --rendertiddler sitemap sitemap.xml text/plain&&
-python3 decodeFilename.py
+cd ..&&
+python3 decodeFilename.py&&
+python3 decodeHTMLlink.py
