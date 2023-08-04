db1404588bd6c6225e786bea5f9c8ed56786ac0a
yamato3010 <yama3to10yama3@gmail.com>
2023-08-04 10:17:59 +0900

    下部にメインメニューに戻るリンクを追加

--- insertNotification.py
+++ insertNotification.py
@@ -11,6 +11,17 @@ def add_text_after_body(html_file, text_to_add):
     with open(html_file, 'w', encoding='utf-8') as file:
         file.write(modified_content)
 
+def add_text_to_body_after(html_file):
+    text_to_add = '<p style="margin-left: 10px;"><a href="https://yamato3010.github.io/wiki#メインページ">メインページに戻る</a></p>'
+    with open(html_file, 'r', encoding='utf-8') as file:
+        content = file.read()
+
+    # bodyタグ内の末尾にテキストを追加
+    modified_content = re.sub(r'(</body>)', r'<p>{}</p>\n\1'.format(text_to_add), content)
+
+    with open(html_file, 'w', encoding='utf-8') as file:
+        file.write(modified_content)
+
 def process_html_files(directory):
     for root, dirs, files in os.walk(directory):
         for file in files:
@@ -18,12 +29,12 @@ def process_html_files(directory):
                 file_path = os.path.join(root, file)
                 base_name = os.path.basename(file_path)
                 filename, _ = os.path.splitext(base_name)
-                text_to_add = '<p style="margin-left: 10px;">このページは<a href="https://yamato3010.github.io/wiki">大和ノ求聞史Wiki</a>の静的バージョンです。記事の内容が最新でない可能性があります。動的版は<a href="https://yamato3010.github.io/wiki#' + filename + '">こちら</a></p>'
+                text_to_add = '<p style="margin-left: 10px;">このページは<a href="https://yamato3010.github.io/wiki">大和ノ求聞史Wiki</a>の静的バージョンです。記事の内容が最新でない可能性があります。動的版は<a href="https://yamato3010.github.io/wiki#' + filename + '">こちら</a>からどうぞ。</p>'
                 add_text_after_body(file_path, text_to_add)
+                add_text_to_body_after(file_path)
                 
 
 if __name__ == "__main__":
     directory_path = "./myfirstwiki/output/static"  # ディレクトリのパスを指定してください
-    text_to_add = '<p style="margin-left: 10px;">このページは<a href="https://yamato3010.github.io/wiki">大和ノ求聞史Wiki</a>の静的バージョンです。記事の内容が最新でない可能性があります。</p>'
 
     process_html_files(directory_path)
